a876c2145906bd36cea4d53ceba87d03640c6c9a
Dennys Valentina Mosquera Maturin <1006168982@academico.icesi.edu.co>
2019-03-13 16:42:43 -0500

    clases

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,14 @@
 package model;
 
+import stack_Linny.LinnyStack;
+
 public class Customer {
 
+	private LinnyStack<Book> shoppingCart;
+	private String userName;
+	
+	public Customer(String userName) {
+		this.userName = userName;
+		
+	}
 }
